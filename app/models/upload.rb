4f7d93ea025cb580d7d93437ed96646f14edf9d5
chaddy81 <chad.bartels@gmail.com>
2011-06-03 01:02:48 -0400

    .

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,4 +1,5 @@
 class Upload < ActiveRecord::Base
+  attr_accessor :upload
   has_attached_file :upload,
       :styles => { :medium => "300x300>", :thumb  => "100x100>" },
       :path => "spiralax/rails/images/:attachment/:id/:style/:filename",
