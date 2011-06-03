29a0796ea6b2660f0b60c3242a568aace68d8e42
chaddy81 <chad.bartels@gmail.com>
2011-06-03 00:11:03 -0400

    .

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,7 +1,7 @@
 class Upload < ActiveRecord::Base
   has_attached_file :upload,
       :styles => { :medium => "300x300>", :thumb  => "100x100>" },
-      :path => "/spiralax/rails/images/:attachment/:id/:style/:filename",
+      :path => "spiralax/rails/images/:attachment/:id/:style/:filename",
       :url => "http://spiralax.com/rails/images/:attachment/:id/:style/:filename",
       :storage => :ftp,
       :ftp_credentials => Rails.root.join('config', 'paperclipftp.yml')
