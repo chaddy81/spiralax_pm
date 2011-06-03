1b161f412f830fdb4afbcfca56b3be0a5feb8fa9
Chad Bartels <chad.bartels@gmail.com>
2011-06-03 15:37:12 -0400

    .

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,5 +1,5 @@
 class Upload < ActiveRecord::Base
   
   has_attached_file :upload,
-      :styles => { :medium => "300x300>", :thumb  => "100x100>" },
+      :styles => { :medium => "300x300>", :thumb  => "100x100>" }
 end
