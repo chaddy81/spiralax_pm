358a47c987b927a53cc1c5e318e63340a52573d8
Chad Bartels <chad.bartels@gmail.com>
2011-06-03 15:53:03 -0400

    Crossing fingers for upload success

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,5 +1,13 @@
 class Upload < ActiveRecord::Base
   
   has_attached_file :upload,
-      :styles => { :medium => "300x300>", :thumb  => "100x100>" }
+      :styles => { :medium => "300x300>", :thumb  => "100x100>" },
+      :path => "/spiralax/rails/images/:attachment/:id/:style/:filename",
+                      :url => "http://spiralax.com/images/:attachment/:id/:style/:filename",
+                      :storage => :ftp,
+                      :ftp_credentials => { :host => 'ftp.spiralax.com', :username => 'revelry2009', :password => 'Keegan0610' },
+                      :ftp_passive_mode => false,
+                      :ftp_timeout => 90,
+                      :ftp_verify_size_on_upload => false,
+                      :ftp_debug_mode => false
 end
