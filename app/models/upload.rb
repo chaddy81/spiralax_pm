0798067fd5ab82f3a72359d664c76b8a0841abc5
Chad Bartels <chad.bartels@gmail.com>
2011-06-03 17:45:15 -0400

    AWS addition

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,13 +1,10 @@
 class Upload < ActiveRecord::Base
   
   has_attached_file :upload,
-      :styles => { :medium => "300x300>", :thumb  => "100x100>" },
-      :path => "spiralax/rails/images/:attachment/:id/:style/:filename",
-                      :url => "http://spiralax.com/images/:attachment/:id/:style/:filename",
-                      :storage => :ftp,
-                      :ftp_credentials => { :host => 'ftp.spiralax.com', :username => 'revelry2009', :password => 'Keegan0610' },
-                      :ftp_passive_mode => false,
-                      :ftp_timeout => 120,
-                      :ftp_verify_size_on_upload => false,
-                      :ftp_debug_mode => false
+    :storage => :s3,
+    :bucket => 'uploads.spiralax.com',
+    :s3_credentials => {
+      :access_key_id => ENV['AKIAIRG5XEIHBB42GFBQ'],
+      :secret_access_key => ENV['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']
+    }
 end
