d0bd7332766025ac45b92c19f0113ce9c4e668a1
Chad Bartels <chad.bartels@gmail.com>
2011-06-03 15:27:11 -0400

    .

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,27 +1,50 @@
 class UploadsController < ApplicationController
+  # GET /Uploads
+  # GET /Uploads.xml
   def index
     @uploads = Upload.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @uploads }
+    end
   end
 
+  # GET /Uploads/1
+  # GET /Uploads/1.xml
   def show
     @upload = Upload.find(params[:id])
-    
+
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @upload }
     end
   end
 
+  # GET /Uploads/new
+  # GET /Uploads/new.xml
   def new
     @upload = Upload.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @upload }
+    end
   end
 
+  # GET /Uploads/1/edit
+  def edit
+    @upload = Upload.find(params[:id])
+  end
+
+  # POST /Uploads
+  # POST /Uploads.xml
   def create
-    @upload = Upload.new(params[:upload])
+    @upload = Upload.new(params[:Upload])
 
     respond_to do |format|
       if @upload.save
-        format.html { redirect_to(@upload, :notice => 'Upload was successfully added.') }
+        format.html { redirect_to(@upload, :notice => 'Upload was successfully created.') }
         format.xml  { render :xml => @upload, :status => :created, :location => @upload }
       else
         format.html { render :action => "new" }
@@ -30,31 +53,31 @@ class UploadsController < ApplicationController
     end
   end
 
-  def edit
-    @upload = Upload.find(params[:id])
-  end
-  
-  def destroy
-    @upload = Upload.find(params[:id])
-    @upload.destroy
-    
-    respond_to do |format|
-      format.html { redirect_to(uploads_url) }
-      format.xml  { head :ok }
-    end
-  end
-
+  # PUT /Uploads/1
+  # PUT /Uploads/1.xml
   def update
-    @upload = Discussion.find(params[:id])
+    @upload = Upload.find(params[:id])
 
     respond_to do |format|
-      if @upload.update_attributes(params[:discussion])
-        format.html { redirect_to(@upload, :notice => 'Discussion was successfully updated.') }
+      if @upload.update_attributes(params[:Upload])
+        format.html { redirect_to(@upload, :notice => 'Upload was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
         format.xml  { render :xml => @upload.errors, :status => :unprocessable_entity }
       end
+    end
+  end
+
+  # DELETE /Uploads/1
+  # DELETE /Uploads/1.xml
+  def destroy
+    @upload = Upload.find(params[:id])
+    @upload.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(uploads_url) }
+      format.xml  { head :ok }
     end
   end
 end
