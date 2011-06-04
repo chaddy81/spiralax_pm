c2cb69bf43d8c856098a16b9ebf75f45d065f83c
chaddy81 <chad.bartels@gmail.com>
2011-06-03 20:52:28 -0400

    New AWS attempt

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,9 +2,10 @@ class Upload < ActiveRecord::Base
   
   has_attached_file :upload,
     :storage => :s3,
-    :bucket => 'UPLOADS.SPIRALAX.COM',
+    :bucket => ENV['S3_BUCKET'],
     :s3_credentials => {
-      :access_key_id => ENV['AKIAISDLMBU4BI4CEINA'],
-      :secret_access_key => ENV['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']
-    }
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    },
+    :path => ":attachment/:id"
 end
