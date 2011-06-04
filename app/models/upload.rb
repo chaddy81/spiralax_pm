0812a6362a0c8fa9a9d969d2cc1feb72006fcc1d
chaddy81 <chad.bartels@gmail.com>
2011-06-03 20:41:37 -0400

    Still working on AWS integration

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,7 +2,7 @@ class Upload < ActiveRecord::Base
   
   has_attached_file :upload,
     :storage => :s3,
-    :bucket => 'uploads.spiralax.com',
+    :bucket => 'UPLOADS.SPIRALAX.COM',
     :s3_credentials => {
       :access_key_id => ENV['AKIAISDLMBU4BI4CEINA'],
       :secret_access_key => ENV['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']
