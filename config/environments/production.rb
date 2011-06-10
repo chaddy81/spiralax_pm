6a7b7b59a02ab0a0ea314dc3c0a94897115e5ee8
Chad Bartels <chad.bartels@gmail.com>
2011-06-10 16:56:49 -0400

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,23 +1,21 @@
 SpiralaxPm::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  # require 'tlsmail'    
-# Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
-# 
-# ActionMailer::Base.delivery_method = :smtp
-# ActionMailer::Base.perform_deliveries = true
-# ActionMailer::Base.raise_delivery_errors = true
-# ActionMailer::Base.smtp_settings = {
-  # :enable_starttls_auto => true,  
-  # :address            => 'smtp.gmail.com',
-  # :port               => 587,
-  # :tls                  => true,
-  # :domain             => 'gmail.com', #you can also use google.com
-  # :authentication     => :plain,
-  # :user_name          => 'chad.bartels@gmail.com',
-  # :password           => 'cbartels6016'
-# }
-
+ require 'tlsmail'       
+ Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)   
+ ActionMailer::Base.delivery_method = :smtp   
+ ActionMailer::Base.perform_deliveries = true   
+ ActionMailer::Base.raise_delivery_errors = true   
+ ActionMailer::Base.smtp_settings = {   
+  :enable_starttls_auto => true,     
+  :address            => 'smtp.gmail.com',   
+  :port               => 587,   
+  :tls                  => true,   
+  :domain             => 'spiralax-pm.heroku.com',    
+  :authentication     => :plain,   
+  :user_name          => 'chad.bartels@gmail.com',   
+  :password           => 'cbartels6016' # for security reasons you can use a environment variable too. (ENV['INFO_MAIL_PASS'])   
+ }   
 
 
   # The production environment is meant for finished, "live" apps.
