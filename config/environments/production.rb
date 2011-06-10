79e459ff0d989274982f8c76fff88bcc677d3432
Chad Bartels <chad.bartels@gmail.com>
2011-06-10 16:24:10 -0400

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,25 @@
 SpiralaxPm::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  require 'tlsmail'    
+Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings = {
+  :enable_starttls_auto => true,  
+  :address            => 'smtp.gmail.com',
+  :port               => 587,
+  :tls                  => true,
+  :domain             => 'gmail.com', #you can also use google.com
+  :authentication     => :plain,
+  :user_name          => 'chad.bartels@gmail.com',
+  :password           => 'cbartels6016'
+}
+
+
+
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
