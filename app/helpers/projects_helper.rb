b1ee702ce46bc7911192a73eb1e45a8ae6a0d508
Chad Bartels <chad.bartels@gmail.com>
2011-06-26 13:29:52 -0400

    .

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -9,9 +9,9 @@ module ProjectsHelper
   
   def select_project
     if session[:current_project].nil?
-      render "projects/select_with_default"
+      render "projects/select_with"
     else
-      render "projects/select_without_default"
+      render "projects/select_without"
     end
   end
 end
