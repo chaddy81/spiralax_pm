d176cfff243dfc9facbcf64333722f49640419e4
chaddy81 <chad.bartels@gmail.com>
2011-07-02 21:06:09 -0400

    .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpiralaxPm::Application.config.secret_token = 'e79b824e8fc093ab5a7e62fce95b47bb9b8eb24e518a3144a63374ace46cd96c19b8e3ee09d0e0cb2202c208f13fa9364f3dd7436b7f9a83719906c94a53c935'
