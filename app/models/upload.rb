2772ab3f89be2285a04832fd114051e87558d241
chaddy81 <chad.bartels@gmail.com>
2011-07-02 21:33:24 -0400

    Jason is a douche

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -5,6 +5,9 @@ class Upload < ActiveRecord::Base
     :styles => { :thumb => '100x100>', :medium => '300x300>'},
     :storage => :s3,
     :bucket => 'uploads.spiralax.com',
-    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+    :s3_credentials => {
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    },
     :path => ":attachment/:id"
 end
