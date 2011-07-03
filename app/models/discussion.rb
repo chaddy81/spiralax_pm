8d8b86960781d3289d000c956b84439247975681
chaddy81 <chad.bartels@gmail.com>
2011-07-02 21:53:36 -0400

    Added models back

---
+++ app/models/discussion.rb
@@ -0,0 +1,15 @@
+class Discussion < ActiveRecord::Base
+  belongs_to :project
+
+	has_many :comments
+	
+	has_attached_file :photo,
+    :styles => { :thumb => '100x100>', :medium => '300x300>'},
+    :storage => :s3,
+    :bucket => 'uploads.spiralax.com',
+    :s3_credentials => {
+      :access_key_id => 'AKIAISDLMBU4BI4CEINA',
+      :secret_access_key => '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'
+    },
+    :path => ":attachment/:id"
+end
---
+++ app/models/milestone.rb
@@ -0,0 +1,5 @@
+class Milestone < ActiveRecord::Base
+  belongs_to :project
+ 
+
+end
